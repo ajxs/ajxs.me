6fb6c90bea48e462c4119b0e13a565c06f54d7f7
Anthony <ajxs@panoptic.online>
2025-06-06 08:37:29 +1000

    Integrate Prettier SQL Plugin

--- src/schema.sql
+++ src/schema.sql
@@ -1,18 +1,20 @@
--- 
+--
 --  Copyright (c) 2020, AJXS.
 --  SPDX-License-Identifier: GPL-3.0-or-later
 --
 --  Authors:
 --     Anthony <ajxs [at] panoptic.online>
--- 
-
-
+--
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS entry;
+
 DROP TABLE IF EXISTS tag;
+
 DROP TABLE IF EXISTS tagged_entry;
+
 DROP TABLE IF EXISTS static_page;
+
 DROP TABLE IF EXISTS static_page_redirect;
 
 CREATE TABLE entry (
@@ -22,11 +24,10 @@ CREATE TABLE entry (
 	body TEXT NOT NULL,
 	date_created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	date_modified DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	date_deleted DATETIME
-	unlisted INTEGER
+	date_deleted DATETIME,
+	unlisted INTEGER,
 	contains_code_blocks INTEGER
 );
-
 
 CREATE TABLE tag (
 	tag_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -35,13 +36,11 @@ CREATE TABLE tag (
 	date_deleted DATETIME
 );
 
-
 CREATE TABLE tagged_entry (
-	tag_id INTEGER NOT NULL REFERENCES tag(tag_id),
-	entry_id INTEGER NOT NULL REFERENCES entry(entry_id) ON DELETE CASCADE,
-	PRIMARY KEY(tag_id, entry_id)
+	tag_id INTEGER NOT NULL REFERENCES tag (tag_id),
+	entry_id INTEGER NOT NULL REFERENCES entry (entry_id) ON DELETE CASCADE,
+	PRIMARY KEY (tag_id, entry_id)
 );
-
 
 CREATE TABLE static_page (
 	page_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -54,9 +53,9 @@ CREATE TABLE static_page (
 	date_deleted DATETIME
 );
 
--- These entries represent static redirect HTML files, designed to perform a client-side 
+-- These entries represent static redirect HTML files, designed to perform a client-side
 -- redirect with a 'refresh' meta directive in the HTML header.
--- These replace the older system of using symlinks to redirect on the server-side due to 
+-- These replace the older system of using symlinks to redirect on the server-side due to
 -- compatibility issues with non-Linux based webhosts.
 CREATE TABLE static_page_redirect (
 	address_from TEXT NOT NULL,
