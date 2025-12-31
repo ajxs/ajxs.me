38b8b0ce382b7e7407f9ec17ffefd41fa9f5135c
Anthony <ajxs@panoptic.online>
2025-12-31 16:44:19 +1100

    μCOM-87 article

--- src/sharedUtilityFunctions.ts
+++ src/sharedUtilityFunctions.ts
@@ -11,9 +11,27 @@ export function convertNameToFilename(name: string): string {
   // This removes all non-legal characters.
   // Refer to: https://www.ietf.org/rfc/rfc3986.txt
   //   Section 2.3: Unreserved Characters.
-  filename = filename.replace(/[^\w\-.~]/g, "");
+  filename = convertToApproximateAsciiRepresentation(filename).replace(
+    /[^\w\-.~]/g,
+    "",
+  );
 
   return filename;
+}
+
+function convertToApproximateAsciiRepresentation(input: string): string {
+  return (
+    input
+      // Specifically replace this character before it gets normalised and stripped
+      // out, as it features prominently in the μCOM-87 article.
+      .replace("μ", "u")
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+      .normalize("NFKD")
+      // strip combining marks.
+      .replace(/[\u0300-\u036f]/g, "")
+      // remove remaining non-ASCII characters.
+      .replace(/[^\x00-\x7F]/g, "")
+  );
 }
 
 export const filterAndSortArticles = (articles: Article[]) =>
